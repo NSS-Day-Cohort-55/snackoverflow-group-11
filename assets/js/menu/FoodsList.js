4bcb85054b02f737f488346911647395dc631240
Houston-Smith <houstonbsmith@gmail.com>
2022-03-25 10:47:30 -0500

    Menu Items Sorted By ID

--- assets/js/menu/FoodsList.js
+++ assets/js/menu/FoodsList.js
@@ -5,6 +5,14 @@ export const FoodList = () => {
     const contentTarget = document.querySelector("main")
     let HTMLString;
     MenuManger.getFoods()
+    .then (unsortedArray => {
+        unsortedArray.sort(function(x, y) 
+            {var firstNumber = new Date(x.menuId),
+                SecondNumber = new Date(y.menuId);
+                if (firstNumber < SecondNumber) return -1;
+                if (firstNumber > SecondNumber) return 1;   return 0; });
+                return(unsortedArray);
+    })
     .then(foodArray => {
         HTMLString = `<div class="row">`
         HTMLString += foodArray.map(food => FoodCard(food)).join('')
